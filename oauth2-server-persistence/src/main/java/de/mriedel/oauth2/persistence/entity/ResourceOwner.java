69dc71c45bec8bdbeab00a2c0dba9eca2d9dabca
maxriedel03 <max@m-riedel.de>
2023-07-22 00:58:14 +0200

    Add Entities with a custom Constraint

---
+++ oauth2-server-persistence/src/main/java/de/mriedel/oauth2/persistence/entity/ResourceOwner.java
@@ -0,0 +1,80 @@
+package de.mriedel.oauth2.persistence.entity;
+
+import de.mriedel.oauth2.persistence.entity.embedabble.Address;
+import de.mriedel.oauth2.persistence.entity.enumurated.Gender;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ResourceOwner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+    @Column(name = "email_verified", nullable = false)
+    private Boolean emailVerified;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "locked", nullable = false)
+    private Boolean banned;
+
+    @Column(name = "deleted", nullable = false)
+    private Boolean deleted;
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Column(name = "date_of_birth")
+    private LocalDateTime dateOfBirth;
+
+    @Column(name = "profile_picture")
+    private String profilePicture;
+
+    @Column(name = "timezone")
+    private String timezone;
+
+    @OneToMany(mappedBy = "resourceOwner")
+    private List<ResourceOwnerClientAuthorizations> clientAuthorizations;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Client> ownedClients;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
---
+++ oauth2-server-persistence/src/main/java/de/mriedel/oauth2/persistence/entity/embedabble/Address.java
@@ -0,0 +1,28 @@
+package de.mriedel.oauth2.persistence.entity.embedabble;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
+public class Address {
+    @Column(name = "country")
+    private String country;
+    @Column(name = "region")
+    private String region;
+    @Column(name = "city")
+    private String city;
+    @Column(name = "postal_code")
+    private String postalCode;
+    @Column(name = "street")
+    private String street;
+    @Column(name = "house_number")
+    private String houseNumber;
+}
